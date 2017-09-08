c4b52d67810c5ac97c3d37d692f8a9e06b54bc81
jbsoliman <jsolimaan@gmail.com>
2017-09-08 13:10:55 -0700

    Update P3.py with correct answer

--- P3.py
+++ P3.py
@@ -1,25 +1,13 @@
 #PROJECTEULER.NET PROBLEM 3. WIP
 
 
-def is_prime(a):
-    return all(a % i for i in xrange(2, a))
-
-check = None
-check1 = None
-largestprime = 0
-
 
 i = 2
-while i < 600851475143:
-    if is_prime(i) == True:
-        if 600851475143 % i == 0:
-            print i
-            largestprime = i
-        else:
-            print i
-            check = True
+n = 600851475143
+while i * i < n:
+    while n%i == 0:
+        n = n/ i
     i = i + 1
 
     
-print "answer is: " + str(largestprime)
-
+print "answer is: " + str(n)
